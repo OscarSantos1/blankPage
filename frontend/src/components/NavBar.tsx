4e2f12d2db98a432c063fd4f7a69a78c9ec84425
OscarSantos1 <ossantos1994@gmail.com>
2023-03-17 03:16:08 -0600

    thinner fields home page ready

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -5,11 +5,11 @@ const NavBar = () => {
   return (
     <div className="fixed z-10 top-0 left-0 right-0 flex flex-row justify-between items-center lg:gap-4 p-2 lg:p-3 md:pb-3 lg:pb-3 bg-white/40 backdrop-blur-sm shadow-sm border-slate-300 fade-in">
       <div className="flex flex-col md:flex-row justify-start md:items-center gap-1 md:gap-4 w-full">
-        <div className="text-base md:text-xl lg:text-2xl text-slate-600">
+        <div className="text-base md:text-xl lg:text-2xl text-sky-600">
           Blank Page:
         </div>
         <div className="text-[9px] md:text-sm lg:text-base md:pt-1 lg:pt-2 text-slate-600">
-          AI generated ideas to kickstart your imagination
+          AI generated ideas to kickstart your creativity
         </div>
       </div>
       <div className="flex items-center gap-1 md:gap-2">
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -20,9 +20,9 @@ export default function Home() {
       <NavBar />
       <div className="flex flex-col justify-center items-center h-screen w-full px-7 backdrop-blur-sm bg-white/20 overflow-scroll fade-in">
         <div className="h-24" />
-        <div className="flex flex-col justify-center h-[30%] w-[90%] md:w-[70%] min-w-[340px] max-w-7xl">
-          <div className="flex flex-col justify-around h-full min-h-[250px] max-h-[500px] w-full mb-20 md:mb-9 roll-in">
-            <div className="flex flex-col h-[200%] md:flex-row md:h-[100%] md:gap-4">
+        <div className="flex flex-col justify-center h-[50%] md:h-[30%] w-[90%] md:w-[70%] min-w-[340px] max-w-7xl">
+          <div className="flex flex-col h-full min-h-[190px] max-h-[500px] w-full mb-20 md:mb-9 roll-in">
+            <div className="flex flex-col md:flex-row h-[200%] md:h-[100%] md:gap-4">
               <Field label={"Time"} content={time} />
               <Field label={"Location"} content={location} />
             </div>
